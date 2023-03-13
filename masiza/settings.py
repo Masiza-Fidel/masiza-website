f04a29384ebfc68070596da57e1e4710dac0eb98
Masiza <fidelmasitsa@gmail.com>
2023-03-13 12:52:54 +0300

    'mama'

--- masiza/settings.py
+++ masiza/settings.py
@@ -126,8 +126,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL= 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'staticfiles'),
 ]
